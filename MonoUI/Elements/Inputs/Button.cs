5ec66a6cd164601cf177867717b178ff3beadf61
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2024-03-30 15:41:26 -0600

    .

---
+++ MonoUI/Elements/Inputs/Button.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoUI.Elements;
+using MonoUI;
+using MonoUI.Elements.Inputs;
+
+namespace JDoddsUI
+{
+    internal class Button : ElementUI
+    {
+        #region Fields
+        
+        #endregion
+
+        #region Properties
+        
+        #endregion
+
+        #region Methods
+        #region Monogame Methods
+        
+        #endregion
+
+        #endregion
+    }
+}
